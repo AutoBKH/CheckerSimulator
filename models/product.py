1e4bea7b4b0b5e8f6d416a621af786ae1a4bae22
orontamir <orontamir@gmail.com>
2020-03-30 21:34:08 +0300

    Checker Simulator first commit

--- models/product.py
+++ models/product.py
@@ -1,14 +1,39 @@
 import asyncio
 import uuid
-from config.confing import DELAY_TIME_SECONDS,PRODUCT_DB_URI,RESPONSE_AFTER_DELAY
+from config.confing import PRODUCT_DB_URI,IMMEDIATE_RESPONSE,DELAY_TIME_SECONDS,DELAYED_RESPONSE
 from aiohttp import ClientSession
+from event_loop import event_loop
+import os
 
 class ProductModel():
-    def __init__(self,instanceid:str,response:str,name:str = None):
+    def __init__(self,instanceid:str, data):
         self.instance_id = instanceid
-        self.response = response
-        self.name = name
+        self.immediate_response = self.get_immediate_response()
+        self.delay_response = self.get_delay_response()
+        self.delay_time_seconds = self.get_delay_time_seconds()
+        self.name = self.get_name(data)
         self.guid = str(uuid.uuid4())
+        self.response = ""
+
+    def get_immediate_response(self):
+        if os.getenv('IMMEDIATE_RESPONSE') is None:
+            return IMMEDIATE_RESPONSE
+        return os.getenv('IMMEDIATE_RESPONSE')
+
+    def get_delay_response(self):
+        if os.getenv('DELAYED_RESPONSE') is None:
+            return DELAYED_RESPONSE
+        return os.getenv('DELAYED_RESPONSE')
+
+    def get_delay_time_seconds(self):
+        if os.getenv('DELAY_TIME_SECONDS') is None:
+            return int(DELAY_TIME_SECONDS)
+        return int(os.getenv('DELAY_TIME_SECONDS'))
+
+    def get_name(self,data):
+        if data['name'] is None:
+            return ""
+        return data['name']
 
     def json(self):
         return {'instanceid': self.instance_id,'responce':  self.response,'name': self.name, 'guid': self.guid}
@@ -18,12 +43,20 @@ class ProductModel():
             sending get request after dakay time
 
         '''
-        await asyncio.sleep(DELAY_TIME_SECONDS)
+        await asyncio.sleep(self.get_delay_time_seconds())
         url = PRODUCT_DB_URI
-        self.response = RESPONSE_AFTER_DELAY
-        async with  ClientSession() as client:
+        self.response = self.get_delay_response()
+        async with ClientSession() as client:
             async with client.get( url, data=self.json()) as response:
                 print(await response.json())
+
+    def get_response(self):
+       if self.get_delay_time_seconds() > 0:
+           asyncio.run_coroutine_threadsafe(self.delay_time(), event_loop)
+       self.response = self.get_immediate_response()
+       return {'message': self.json()}, 202
+
+
 
 
 
--- resources/product.py
+++ resources/product.py
@@ -2,7 +2,7 @@ import asyncio
 
 from flask_restful import Resource, reqparse
 from models.product import ProductModel
-from config.confing import UPDATE_RESPONSE, DELETE_RESPONSE
+
 from event_loop import event_loop
 
 class Product(Resource):
@@ -27,25 +27,16 @@ class Product(Resource):
     def delete(self, instanceid):
         try:
             data = Product.parser.parse_args()
-            if data['name'] is None:
-                delete_product = ProductModel(instanceid, DELETE_RESPONSE)
-            else:
-                delete_product = ProductModel(instanceid, DELETE_RESPONSE, data['name'])
-            if DELETE_RESPONSE == 'ACCEPTED':
-                asyncio.run_coroutine_threadsafe(delete_product.delay_time(),event_loop)
-            return {'message': delete_product.json()}, 200
+            delete_product = ProductModel(instanceid, data)
+            return delete_product.get_response()
+
         except Exception as e:
             return {"message": str(e)}, 500
 
     def put(self, instanceid):
         try:
             data = Product.parser.parse_args()
-            if data['name'] is None:
-                update_product = ProductModel(instanceid, UPDATE_RESPONSE)
-            else:
-                update_product = ProductModel(instanceid, UPDATE_RESPONSE, data['name'])
-            if UPDATE_RESPONSE == 'ACCEPTED':
-                asyncio.run_coroutine_threadsafe(update_product.delay_time(),event_loop)
-            return {'message': update_product.json()}, 200
+            update_product = ProductModel(instanceid, data)
+            return update_product.get_response()
         except:
             return {"message": "An error occurred when updating product."}, 500
